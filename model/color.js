ec1484973e8814d27f3736db5ad7a25ef17f4298
Yusuke Kawasaki <u-suke@kawa.net>
2016-06-15 20:51:16 +0900

    color is a filter but not a class

--- model/color.js
+++ model/color.js
@@ -1,19 +1,9 @@
 // color.js
 
 function Color(val) {
-  if (!(this instanceof Color)) return new Color(val);
-  this[0] = val;
-}
-
-Color.prototype.length = 1;
-
-Color.prototype.toString = toString;
-
-Color.prototype.toJSON = toString;
-
-function toString() {
-  var val = this[0];
-  if (val && val.search(/^#[0-9A-Fa-f]{3,4}$/) === 0) {
+  if (!val) return val;
+  val += "";
+  if (val.search(/^#[0-9A-Fa-f]{3,4}$/) === 0) {
     // #RGB -> #RRGGBB
     // #ARGB -> #AARRGGBB
     val = val.split("").map(function(c) {
